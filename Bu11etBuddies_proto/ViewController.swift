26a661c5eaf995f7fc237bc13c90fe608b60a150
HideYa-bu11ets <104835928+HideYa-bu11ets@users.noreply.github.com>
2023-09-11 12:11:41 +0900

    Initial Commit

---
+++ Bu11etBuddies_proto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bu11etBuddies_proto
+//
+//  Created by 沼田英也 on 2023/09/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
