2e4c9dbf70a9ba59af0da61f1ad2e974a5b5843c
SotaSakuragi <ebi.ebi.kani.kani.ikura@gmail.com>
2023-09-12 18:12:52 +0900

    backgroundを木目調にしたよ

--- Bu11etBuddies_proto/MainScreenViewController.swift
+++ Bu11etBuddies_proto/MainScreenViewController.swift
@@ -13,7 +13,6 @@ class MainScreenViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
