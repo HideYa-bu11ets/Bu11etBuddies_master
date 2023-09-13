03cea64ba423b0f9950ae3131c183243b7439cd8
SotaSakuragi <ebi.ebi.kani.kani.ikura@gmail.com>
2023-09-13 15:00:48 +0900

    searchareaのcellの設定完了

--- Bu11etBuddies_proto/SeachAreaViewController.swift
+++ Bu11etBuddies_proto/SeachAreaViewController.swift
@@ -7,23 +7,70 @@
 
 import UIKit
 
-class SeachAreaViewController: UIViewController {
-
+class SeachAreaViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    
+    @IBOutlet weak var searchAreaTextField: UITextField!
+    @IBOutlet weak var areaTableView: UITableView!
+    var searchAreaCells : [[String: String]] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        areaTableView.delegate = self
+        areaTableView.dataSource = self
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func searchButton(_ sender: Any) {
     }
-    */
-
+    
+    // MARK: - Cell
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchAreaCells.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = areaTableView.dequeueReusableCell(withIdentifier: "AreaCell", for: indexPath)
+        
+        //        店舗名
+        let storeNameLabel = cell.viewWithTag(1) as! UILabel
+        storeNameLabel.text = searchAreaCells[indexPath.row]["エリア"]
+        
+        //        店舗画像
+        let storeImageView = cell.viewWithTag(4) as! UIImageView
+        if let imageUrlString = searchAreaCells[indexPath.row]["img_list"],
+           let imageUrl = URL(string: imageUrlString),
+           !imageUrlString.contains("amazon") {
+            storeImageView.kf.setImage(with: imageUrl)
+        } else {
+            storeImageView.image = UIImage(named: "noimage")
+        }
+        
+        
+        //        住所
+        let addressLabel = cell.viewWithTag(3) as! UILabel
+        addressLabel.text = searchAreaCells[indexPath.row]["住所"]
+        
+        
+        //        料金
+        let feeLabel = cell.viewWithTag(3) as! UILabel
+        feeLabel.text = searchAreaCells[indexPath.row]["定例会料金・利用料金"]
+        
+        return cell
+    }
+    
+    //    セルの高さ
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return self.view.frame.height * 2 / 3
+    }
+    
+    
 }
+
