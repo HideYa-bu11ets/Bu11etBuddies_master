37b0cb4310972d97d023e260545c53618fb7de55
SotaSakuragi <ebi.ebi.kani.kani.ikura@gmail.com>
2023-09-16 10:25:20 +0900

    動画からの遷移できた

--- Bu11etBuddies_proto/animeViewController.swift
+++ Bu11etBuddies_proto/animeViewController.swift
@@ -11,46 +11,48 @@ import AVFoundation
 
 class animeViewController: UIViewController {
 
+    var player: AVPlayer?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        playLocalVideo()
-        print("表示")
 
-        // Do any additional setup after loading the view.
+        // 動画終了時の通知を追加
+        NotificationCenter.default.addObserver(self, selector: #selector(playerDidReachEnd), name: .AVPlayerItemDidPlayToEndTime, object: nil)
+
+        playLocalVideo()
     }
-    
-    var playerLooper: AVPlayerLooper?
-    let player = AVQueuePlayer()
 
     func playLocalVideo() {
         if let path = Bundle.main.path(forResource: "銃痕2_smg", ofType: "mp4") {
             let videoURL = URL(fileURLWithPath: path)
-            let playerItem = AVPlayerItem(url: videoURL)
-            
-            playerLooper = AVPlayerLooper(player: player, templateItem: playerItem)
-            
+
+            player = AVPlayer(url: videoURL)
             let playerViewController = AVPlayerViewController()
             playerViewController.player = player
-            
-            // コントロールバーを非表示にする
             playerViewController.showsPlaybackControls = false
 
-            present(playerViewController, animated: true) {
-                playerViewController.player?.play()
-            }
+            // タップジェスチャの追加
+            let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+            playerViewController.view.addGestureRecognizer(tapGesture)
+
+            // 子ビューコントローラとしてAVPlayerViewControllerを追加
+            addChild(playerViewController)
+            view.addSubview(playerViewController.view)
+            playerViewController.didMove(toParent: self)
+
+            player?.play()
         }
     }
 
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func playerDidReachEnd(note: NSNotification) {
+        player?.seek(to: CMTime.zero)
+        player?.play()
     }
-    */
 
+    @objc func handleTap() {
+        if let openingVC = storyboard?.instantiateViewController(withIdentifier: "OpeningViewControllerID") as? OpeningViewController {
+            navigationController?.pushViewController(openingVC, animated: true)
+        }
+        player?.pause()
+    }
 }
