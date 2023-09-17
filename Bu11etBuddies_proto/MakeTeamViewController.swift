3ee65c9ae2d2bad841fb4f47e52b5b57a64d2ac0
SotaSakuragi <ebi.ebi.kani.kani.ikura@gmail.com>
2023-09-17 09:50:26 +0900

    teamメンバー表示作成

--- Bu11etBuddies_proto/MakeTeamViewController.swift
+++ Bu11etBuddies_proto/MakeTeamViewController.swift
@@ -6,24 +6,74 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
 
-class MakeTeamViewController: UIViewController {
+class MakeTeamViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
 
+    @IBOutlet weak var teamShowTableView: UITableView!
+    var profileDateDic: [String: String] = [:]
+    var teamMemberCells: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        teamShowTableView.delegate = self
+        teamShowTableView.dataSource = self
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let userDefaults = UserDefaults.standard
+        //中に何かあれば辞書に
+        if userDefaults.object(forKey: "add") != nil {
+            profileDateDic = userDefaults.object(forKey: "add") as! [String: String]
+            print(profileDateDic["team"])
+        }
+        fetchTeamMembers()
     }
-    */
+    
+    func fetchTeamMembers() {
+        guard let teamName = profileDateDic["team"] else { return }
+
+        let ref = Database.database().reference()
+        ref.child("teamDate").child(teamName).observe(.value) { (snapshot) in
+            var members: [String] = []
+            for child in snapshot.children {
+                if let snap = child as? DataSnapshot, let dict = snap.value as? [String: String], let tagName = dict["tagName"] {
+                    members.append(tagName)
+                }
+            }
+            self.teamMemberCells = members
+            self.teamShowTableView.reloadData()
+        }
+    }
+
+
+
+// MARK: - Navigation
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return teamMemberCells.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = teamShowTableView.dequeueReusableCell(withIdentifier: "TCells", for: indexPath)
+        let memberNameLabel = cell.viewWithTag(1) as! UILabel
+        memberNameLabel.text = teamMemberCells[indexPath.row]
+        return cell
+    }
+
+    
+    //    セルの高さ
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return self.view.frame.height * 2 / 15
+    }
 
 }
